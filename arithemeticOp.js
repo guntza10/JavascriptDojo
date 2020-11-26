4ee86cf2c7f218064666b779e8bbbc844be42133
guntza10 <guntza22@hotmail.com>
2020-11-27 00:16:55 +0700

    arithemetic operators

---
+++ arithemeticOp.js
@@ -0,0 +1,11 @@
+var addition = 2 + 2;
+var substraction = 3 - 90;
+var division = 10 / 5;
+var multiplication = 3 * 30;
+var mod = 10 % 4;
+
+console.log(addition);
+console.log(substraction);
+console.log(division);
+console.log(multiplication);
+console.log(mod);
