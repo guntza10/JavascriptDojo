182df20ae854b25348dae1cc6a02699b39c6c120
guntza10 <guntza22@hotmail.com>
2020-12-18 23:04:27 +0700

    update

--- module/Animal.js
+++ module/Animal.js
@@ -14,3 +14,7 @@ default export
 - default export สามารถมีได้แค่ 1 อันเท่านั้นใน module
 - ตอน import สามารถเปลี่ยนชื่อได้ ไม่จำเป็นต้องเป็นชื่อเดียวกันกับใน export
 */
+
+/*
+- https://javascript.info/import-export
+ */
